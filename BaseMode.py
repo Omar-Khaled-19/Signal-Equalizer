f015d041b41f5f08e07cee81fdb67594700bb594
Omar-Khaled-19 <omarkhaled191104@gmail.com>
2023-11-12 22:30:43 +0200

    Plotting is Done with the new architecture

--- BaseMode.py
+++ BaseMode.py
@@ -1,21 +1,90 @@
 from abc import ABC, abstractmethod # Used  to declare abstract class and pure virtual functions
-import Modes
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QFileDialog
+import numpy as np
+from scipy.io import wavfile
+import bisect
+
+
 class BaseMode(ABC):
-    def __init__(self, input_time_graph, output_time_graph, frequency_graph, input_spectro, output_spectro, slider1, slider2, slider3, slider4):
-        self.time_graph = Modes.TimeGraph(input_time_graph, output_time_graph)
+    def __init__(self, ui, input_time_graph, output_time_graph, frequency_graph, input_spectro, output_spectro, slider1, slider2, slider3, slider4):
+        self.ui = ui
+        self.input_graph = input_time_graph
+        self.output_graph = output_time_graph
         self.frequency_graph = frequency_graph
         self.input_spectrogram = input_spectro
         self.output_spectrogram = output_spectro
-        self.time_domain_X_coordinates = None
-        self.time_domain_Y_coordinates = None
+        self.time_domain_X_coordinates = []
+        self.time_domain_Y_coordinates = []
         self.slider1 = slider1
         self.slider2 = slider2
         self.slider3 = slider3
         self.slider4 = slider4
 
+        self.X_Points_Plotted = 0
+        self.paused = False
+        self.speed = 10
+        self.stopped = False
+        self.player = QMediaPlayer()
+
     @abstractmethod
     def modify_frequency(self, value: int):
         pass
+    
     def load_signal(self):
-        # Load should be implemented for each tab rather than for a certain graph type, and the function should handle 2 cases(.wav, .hea)
-        pass
+        self.input_graph.clear()
+        File_Path, _ = QFileDialog.getOpenFileName(None, "Browse Signal", "", "All Files (*)")
+        
+        self.sample_rate, self.audio_data = wavfile.read(File_Path)
+
+        self.time_domain_X_coordinates = np.arange(len(self.audio_data)) / self.sample_rate
+        self.time_domain_Y_coordinates = self.audio_data
+        
+        self.player.setMedia(QMediaContent(QUrl.fromLocalFile(File_Path)))
+
+        self.plot_signal()
+        
+            
+    def plot_signal(self):
+        self.input_graph.setLimits(xMin=0, xMax=float('inf'))
+        self.data_line = self.input_graph.plot(self.time_domain_X_coordinates[:1], self.time_domain_Y_coordinates[:1],pen="g")
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(100)
+        self.timer.timeout.connect(self.update_plot_data)
+        self.timer.start()
+        self.player.play()
+
+    def update_plot_data(self):
+        if not self.paused and not self.stopped:             
+            sound_position = self.player.position()
+            sound_duration = self.player.duration()
+            progress = sound_position / sound_duration
+
+            target_x = int(progress * max(self.time_domain_X_coordinates))
+            target_index = bisect.bisect_left(self.time_domain_X_coordinates, target_x)
+
+            self.input_graph.getViewBox().setXRange(target_x - 4, target_x)
+            self.data_line.setData(self.time_domain_X_coordinates[:target_index], self.time_domain_Y_coordinates[:target_index])
+
+    def toggle_pause(self):
+        self.paused = not self.paused
+
+    def reset(self):
+        self.X_Points_Plotted = 0
+
+    def update_speed(self,slider):
+        self.speed = 10*slider.value()
+
+    def stop(self):
+        self.stopped = True
+        self.input_graph.clear()
+        self.input_graph.getViewBox().setXRange(0,100)
+        self.X_Points_Plotted = 0
+
+    def zoomin(self):
+        self.input_graph.getViewBox().scaleBy((0.9, 0.9))
+
+    def zoomout(self):
+        self.input_graph.getViewBox().scaleBy((1.1,1.1))
--- Modes.py
+++ Modes.py
@@ -1,36 +1,99 @@
 import BaseMode
+from PyQt5.QtWidgets import QFileDialog
+import wfdb, numpy as np
+from PyQt5 import QtGui
 
 class UniformMode(BaseMode.BaseMode):
 
-    def __init__(self):
-        pass
+    def __init__(self, ui, input_time_graph, output_time_graph, frequency_graph, input_spectro, output_spectro, slider1, slider2, slider3, slider4):
+        super().__init__(ui, input_time_graph, output_time_graph, frequency_graph, input_spectro, output_spectro, slider1, slider2, slider3, slider4)
 
     def modify_frequency(self, value: int):
         pass
+
+    def toggle_pause(self):
+        super().toggle_pause()
+        icon = QtGui.QIcon()
+        if not self.paused:
+            icon.addPixmap(QtGui.QPixmap("Assets/pause.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+            self.ui.Uniform_Range_Play_Pause_Button.setIcon(icon)
+        else:
+            icon.addPixmap(QtGui.QPixmap("Assets/play (1).png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+            self.ui.Uniform_Range_Play_Pause_Button.setIcon(icon)
 
 
 
 class MusicalMode(BaseMode.BaseMode):
 
-    def __init__(self):
-        pass
+    def __init__(self, ui, input_time_graph, output_time_graph, frequency_graph, input_spectro, output_spectro, slider1, slider2, slider3, slider4):
+        super().__init__(ui, input_time_graph, output_time_graph, frequency_graph, input_spectro, output_spectro, slider1, slider2, slider3, slider4)
 
     def modify_frequency(self, value: int):
         pass
+
+    def toggle_pause(self):
+        super().toggle_pause()
+        icon = QtGui.QIcon()
+        if not self.paused:
+            icon.addPixmap(QtGui.QPixmap("Assets/pause.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+            self.ui.Musical_Instruments_Play_Pause_Button.setIcon(icon)
+        else:
+            icon.addPixmap(QtGui.QPixmap("Assets/play (1).png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+            self.ui.Musical_Instruments_Play_Pause_Button.setIcon(icon)
 
 
 class AnimalMode(BaseMode.BaseMode):
 
-    def __init__(self):
-        pass
+    def __init__(self, ui, input_time_graph, output_time_graph, frequency_graph, input_spectro, output_spectro, slider1, slider2, slider3, slider4):
+        super().__init__(ui, input_time_graph, output_time_graph, frequency_graph, input_spectro, output_spectro, slider1, slider2, slider3, slider4)
 
     def modify_frequency(self, value: int):
         pass
 
+    def toggle_pause(self):
+        super().toggle_pause()
+        icon = QtGui.QIcon()
+        if not self.paused:
+            icon.addPixmap(QtGui.QPixmap("Assets/pause.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+            self.ui.Animals_Sounds_Play_Pause_Button.setIcon(icon)
+        else:
+            icon.addPixmap(QtGui.QPixmap("Assets/play (1).png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+            self.ui.Animals_Sounds_Play_Pause_Button.setIcon(icon)
 
 class ECGMode(BaseMode.BaseMode):
 
-    def __init__(self):
-        pass
+    def __init__(self, ui, input_time_graph, output_time_graph, frequency_graph, input_spectro, output_spectro, slider1, slider2, slider3, slider4):
+        super().__init__(ui, input_time_graph, output_time_graph, frequency_graph, input_spectro, output_spectro, slider1, slider2, slider3, slider4)
+    
     def modify_frequency(self, value: int):
         pass
+
+    def load_signal(self):
+        self.input_graph.clear()
+        File_Path, _ = QFileDialog.getOpenFileName(None, "Browse Signal", "", "All Files (*)")
+        if File_Path:
+            Record = wfdb.rdrecord(File_Path[:-4])
+            self.time_domain_Y_coordinates = list(Record.p_signal[:, 0])
+            self.time_domain_X_coordinates = list(np.arange(len(self.time_domain_Y_coordinates)))
+            self.stopped = False
+            self.plot_signal()
+
+    def update_plot_data(self):
+        if not self.paused and not self.stopped:
+            self.X_Points_Plotted += self.speed
+            self.data_line.setData(self.time_domain_X_coordinates[0 : self.X_Points_Plotted + 1], 
+                                self.time_domain_Y_coordinates[0 : self.X_Points_Plotted + 1])
+            
+            self.input_graph.getViewBox().setXRange(max(self.time_domain_X_coordinates[0: self.X_Points_Plotted + 1]) - 200, 
+                                                    max(self.time_domain_X_coordinates[0: self.X_Points_Plotted + 1]))
+        
+    def toggle_pause(self):
+        super().toggle_pause()
+        icon = QtGui.QIcon()
+        if not self.paused:
+            icon.addPixmap(QtGui.QPixmap("Assets/pause.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+            self.ui.ECG_Abnormalities_Play_Pause_Button.setIcon(icon)
+        else:
+            icon.addPixmap(QtGui.QPixmap("Assets/play (1).png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+            self.ui.ECG_Abnormalities_Play_Pause_Button.setIcon(icon)
+
